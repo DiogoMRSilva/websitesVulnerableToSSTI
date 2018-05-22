26d3b418928c6acba8c9621bb7157bd34ac33a45
DiogoMRSilva <diogo.m.r.silva@tecnico.ulisboa.pt>
2018-05-22 16:11:13 +0100

    Added page with only the vulnerable servers

--- RunAll/makeRootWithAllLinks.py
+++ RunAll/makeRootWithAllLinks.py
@@ -37,9 +37,43 @@ html+="</table></body></html>"
 from flask import *
 app = Flask(__name__)
 
-@app.route('/',methods=['GET', 'POST'])
+@app.route('/all',methods=['GET', 'POST'])
 def base():
     return html
+
+@app.route('/',methods=['GET','POST'])
+def other():
+    return '''<!DOCTYPE html><html><head><style>table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+}
+
+td, th {
+    border: 0.8px solid #dddddd;
+    text-align: left;
+    padding: 1px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style></head><body><table><tr><th><a href="http://127.0.0.1:5000">jinja2</a></td><td>Python</td></tr>
+<tr><th><a href="http://127.0.0.1:5001">Mako</a></td><td>Python</td></tr>
+<tr><th><a href="http://127.0.0.1:5002">Tornado</a></td><td>Python</td></tr>
+<tr><th><a href="http://127.0.0.1:5020">Smarty</a></td><td>PHP</td></tr>
+<tr><th><a href="http://127.0.0.1:5021">Smarty(secure mode)</a></td><td>PHP</td></tr>
+<tr><th><a href="http://127.0.0.1:5022">Twig</a></td><td>PHP</td></tr>
+<tr><th><a href="http://127.0.0.1:5051">FreeMarker</a></td><td>Java</td></tr>
+<tr><th><a href="http://127.0.0.1:5052">Velocity</a></td><td>Java</td></tr>
+<tr><th><a href="http://127.0.0.1:5053">Thymeleaf</a></td><td>Java</td></tr>
+<tr><th><a href="http://127.0.0.1:5061">jade</a></td><td>Nodejs</td></tr>
+<tr><th><a href="http://127.0.0.1:5062">Nunjucks</a></td><td>JavaScript</td></tr>
+<tr><th><a href="http://127.0.0.1:5063">doT</a></td><td>JavaScript</td></tr>
+<tr><th><a href="http://127.0.0.1:5066">EJS</a></td><td>JavaScript</td></tr>
+<tr><th><a href="http://127.0.0.1:5068">vuejs</a></td><td>JavaScript</td></tr>
+<tr><th><a href="http://127.0.0.1:5080">Slim</a></td><td>Ruby</td></tr>
+<tr><th><a href="http://127.0.0.1:5081">ERB</a></td><td>Ruby</td></tr>
+</table></body></html>'''
 
 
 if __name__=="__main__":
