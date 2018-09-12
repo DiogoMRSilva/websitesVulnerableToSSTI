6116fbe27c7d362ff6cc73f4b03f3f20a45b9565
DiogoMRSilva <diogo.m.r.silva@tecnico.ulisboa.pt>
2018-09-12 14:47:49 +0100

    Added test where the input is inserted in the midle of code but inside one string

---
+++ specialTests/insertedInMidleCodeText/runInDocker.sh
@@ -0,0 +1,9 @@
+if [ $# -eq 0 ]
+  then
+    IP="127.0.0.1" 
+else
+    IP=$1
+fi
+
+docker build -t input-inserted-middle-code-text .
+docker run -d -i -p $IP:6005:5001 -w /home input-inserted-middle-code-text sh ./run.sh 
