0417b12fd28bc9cd840903b17512defecb51031c
Diogo Silva <diogomiresi@gmail.com>
2021-11-12 21:34:46 +0000

    Add more python eval vulnability examples
    Signed-off-by: Diogo Silva <diogomiresi@gmail.com>

--- python/python-eval/src/server.py
+++ python/python-eval/src/server.py
@@ -1,10 +1,40 @@
-# for this we need to install flask
 from flask import *
 app = Flask(__name__)
 
-@app.route('/',methods=['GET', 'POST'])
+template = """<!DOCTYPE html><html><body>\
+    <h1>Online Calculator</h1>\
+    <form action="/" method="post">\
+      expression:<br>\
+      <input type="text" name="expression" value="">\
+      <input type="submit" value="Submit">\
+    </form>
+    
+    <form action="/evalDoubleQuoted" method="post">\
+      expression:<br>\
+      <input type="text" name="expression" value="">\
+      <input type="submit" value="Submit">\
+    </form>
+
+    <form action="/evalQuoted" method="post">\
+      expression:<br>\
+      <input type="text" name="expression" value="">\
+      <input type="submit" value="Submit">\
+    </form>
+
+    <form action="/evalMiddleMathOp" method="post">\
+      expression:<br>\
+      <input type="text" name="expression" value="">\
+      <input type="submit" value="Submit">\
+    </form>
+    
+    <h2>%s </h2></body></html>"""
+
+@app.route('/',methods=['GET'])
+def baseGet():
+    return template % "SEND INPUT"
+
+@app.route('/',methods=['POST'])
 def base():
-    expression = "1+1"
     if request.method == 'POST':
       if request.form['expression']:
         expression = request.form['expression']
@@ -12,19 +42,52 @@ def base():
     try:
       result = eval(expression)
     except:
-      resust = "error"
+      result = "error"
 
-    
-  
-    template = '<!DOCTYPE html><html><body>\
-    <h1>Online Calculator</h1>\
-    <form action="/" method="post">\
-      expression:<br>\
-      <input type="text" name="expression" value="">\
-      <input type="submit" value="Submit">\
-    </form><h2>'+expression+' = %s </h2></body></html>' % result
-    return template
+    html = template % result
+    return html
 
+@app.route('/evalDoubleQuoted',methods=['POST'])
+def evalDoubleQuoted():
+    if request.method == 'POST':
+      if request.form['expression']:
+        expression = request.form['expression']
+    result = ""
+    try:
+      result = eval('"lala" + "%s"' % expression)
+    except:
+      result = "error"
+
+    html = template % result
+    return html
+
+@app.route('/evalQuoted',methods=['POST'])
+def evalQuoted():
+    if request.method == 'POST':
+      if request.form['expression']:
+        expression = request.form['expression']
+    result = ""
+    try:
+      result = eval("'lala' + '%s'" % expression)
+    except:
+      result = "error"
+
+    html = template % result
+    return html
+
+@app.route('/evalMiddleMathOp',methods=['POST'])
+def evalMiddleMathOp():
+    if request.method == 'POST':
+      if request.form['expression']:
+        expression = request.form['expression']
+    result = ""
+    try:
+      result = eval("%s % 7" % expression)
+    except:
+      result = "error"
+
+    html = template % result
+    return html
 
 if __name__=="__main__":
 	app.run("0.0.0.0",port = 5004,debug=False)
--- python/python-exec/src/server.py
+++ python/python-exec/src/server.py
@@ -1,29 +1,32 @@
-# for this we need to install flask
 from flask import *
 app = Flask(__name__)
 
-@app.route('/',methods=['GET', 'POST'])
+template = '<!DOCTYPE html><html><body>\
+    <h1>Online Calculator</h1>\
+    <form action="/" method="post">\
+      expression:<br>\
+      <input type="text" name="expression" value="">\
+      <input type="submit" value="Submit">\
+    </form><h2>%s </h2></body></html>'
+
+
+@app.route('/',methods=['GET'])
 def base():
-    expression = "1+1"
-    if request.method == 'POST':
-      if request.form['expression']:
-        expression = request.form['expression']
+    return template % "SEND INPUT"
+
+@app.route('/',methods=['POST'])
+def base2():
+    expression = ""
+    if request.form['expression']:
+      expression = request.form['expression']
     result = ""
     try:
       exec("result =("+expression+")")
     except:
       result = "error"
 
-    
-  
-    template = '<!DOCTYPE html><html><body>\
-    <h1>Online Calculator</h1>\
-    <form action="/" method="post">\
-      expression:<br>\
-      <input type="text" name="expression" value="">\
-      <input type="submit" value="Submit">\
-    </form><h2>'+expression+' = %s </h2></body></html>' % result
-    return template
+    html = template % result
+    return html
 
 
 if __name__=="__main__":
